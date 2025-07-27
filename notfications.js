c7b4a9daac853a027906c124c7b8445885a95ab2
khaled010012 <khaledahmed140140@gmail.com>
2025-07-27 22:07:25 +0300

    Update notfications.js

--- notfications.js
+++ notfications.js
@@ -1,4 +1,1 @@
-// External script placeholder for Crypto Exchanger PHP notifications
-// Will be dynamically updated later after extension is approved
-
 console.log("Notifications script loaded");
